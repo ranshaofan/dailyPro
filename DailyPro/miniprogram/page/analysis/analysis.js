87e1808555c3dcf8974a23490d74a18f236d4301
ranshaofan <ranshaofan1009@163.com>
2023-06-25 18:10:19 +0800

    pages

--- DailyPro/miniprogram/page/analysis/analysis.js
+++ DailyPro/miniprogram/page/analysis/analysis.js
@@ -6,254 +6,6 @@
 //     }
 //   },
 
-//   data: {
-//     list: [{
-//       id: 'api',
-//       name: '开放接口',
-//       open: false,
-//       pages: [{
-//         zh: '微信登录',
-//         url: 'login/login'
-//       }, {
-//         zh: '获取用户信息',
-//         url: 'get-user-info/get-user-info'
-//       }, {
-//         zh: '发起支付',
-//         url: 'request-payment/request-payment'
-//       }, {
-//         zh: '跳转',
-//         url: 'jump/jump'
-//       }, {
-//         zh: '转发',
-//         url: 'share/share'
-//       }, {
-//         zh: '转发按钮',
-//         url: 'share-button/share-button'
-//       }, {
-//         zh: '客服消息',
-//         url: 'custom-message/custom-message'
-//       }, {
-//         zh: '订阅消息',
-//         url: 'subscribe-message/subscribe-message'
-//       }, {
-//         zh: '收货地址',
-//         url: 'choose-address/choose-address'
-//       }, {
-//         zh: '获取发票抬头',
-//         url: 'choose-invoice-title/choose-invoice-title'
-//       }, {
-//         zh: '生物认证',
-//         url: 'soter-authentication/soter-authentication'
-//       }, {
-//         zh: '设置',
-//         url: 'setting/setting'
-//       }]
-//     }, {
-//       id: 'page',
-//       name: '界面',
-//       open: false,
-//       pages: [{
-//         zh: '设置界面标题',
-//         url: 'set-navigation-bar-title/set-navigation-bar-title'
-//       }, {
-//         zh: '标题栏加载动画',
-//         url: 'navigation-bar-loading/navigation-bar-loading'
-//       }, {
-//         zh: '设置TabBar',
-//         url: '@set-tab-bar'
-//       }, {
-//         zh: '页面跳转',
-//         url: 'navigator/navigator'
-//       }, {
-//         zh: '下拉刷新',
-//         url: 'pull-down-refresh/pull-down-refresh'
-//       }, {
-//         zh: '创建动画',
-//         url: 'animation/animation'
-//       }, {
-//         zh: '创建绘画',
-//         url: 'canvas/canvas'
-//       }, {
-//         zh: '显示操作菜单',
-//         url: 'action-sheet/action-sheet'
-//       }, {
-//         zh: '显示模态弹窗',
-//         url: 'modal/modal'
-//       }, {
-//         zh: '页面滚动',
-//         url: 'page-scroll/page-scroll'
-//       }, {
-//         zh: '显示消息提示框',
-//         url: 'toast/toast'
-//       }, {
-//         zh: '获取WXML节点信息',
-//         url: 'get-wxml-node-info/get-wxml-node-info'
-//       }, {
-//         zh: 'WXML节点布局相交状态',
-//         url: 'intersection-observer/intersection-observer'
-//       }]
-//     }, {
-//       id: 'device',
-//       name: '设备',
-//       open: false,
-//       pages: [{
-//         zh: '获取手机网络状态',
-//         url: 'get-network-type/get-network-type'
-//       }, {
-//         zh: '监听手机网络变化',
-//         url: 'on-network-status-change/on-network-status-change'
-//       }, {
-//         zh: '获取手机系统信息',
-//         url: 'get-system-info/get-system-info'
-//       }, {
-//         zh: '获取手机设备电量',
-//         url: 'get-battery-info/get-battery-info'
-//       }, {
-//         zh: '监听重力感应数据',
-//         url: 'on-accelerometer-change/on-accelerometer-change'
-//       }, {
-//         zh: '监听罗盘数据',
-//         url: 'on-compass-change/on-compass-change'
-//       }, {
-//         zh: '打电话',
-//         url: 'make-phone-call/make-phone-call'
-//       }, {
-//         zh: '扫码',
-//         url: 'scan-code/scan-code'
-//       }, {
-//         zh: '剪切板',
-//         url: 'clipboard-data/clipboard-data'
-//       }, {
-//         zh: '蓝牙',
-//         url: 'bluetooth/bluetooth'
-//       }, {
-//         zh: 'iBeacon',
-//         url: 'ibeacon/ibeacon'
-//       }, {
-//         zh: '屏幕亮度',
-//         url: 'screen-brightness/screen-brightness'
-//       }, {
-//         zh: '用户截屏事件',
-//         url: 'capture-screen/capture-screen'
-//       }, {
-//         zh: '振动',
-//         url: 'vibrate/vibrate'
-//       }, {
-//         zh: '手机联系人',
-//         url: 'add-contact/add-contact'
-//       }, {
-//         zh: 'Wi-Fi',
-//         url: 'wifi/wifi'
-//       }]
-//     }, {
-//       id: 'performance',
-//       name: '性能',
-//       open: false,
-//       pages: [{
-//         zh: '获取性能数据',
-//         url: 'get-performance/get-performance'
-//       }]
-//     }, {
-//       id: 'network',
-//       name: '网络',
-//       open: false,
-//       pages: [{
-//         zh: '发起一个请求',
-//         url: 'request/request'
-//       }, {
-//         zh: 'WebSocket',
-//         url: 'web-socket/web-socket'
-//       }, {
-//         zh: '上传文件',
-//         url: 'upload-file/upload-file'
-//       }, {
-//         zh: '下载文件',
-//         url: 'download-file/download-file'
-//       }, {
-//         zh: 'UDPSocket',
-//         url: 'udp-socket/udp-socket'
-//       }, {
-//         zh: 'mDNS',
-//         url: 'mdns/mdns'
-//       }]
-//     }, {
-//       id: 'media',
-//       name: '媒体',
-//       open: false,
-//       pages: [{
-//         zh: '图片',
-//         url: 'image/image'
-//       }, {
-//         zh: '音频',
-//         url: 'audio/audio'
-//       }, {
-//         zh: '录音',
-//         url: 'voice/voice'
-//       }, {
-//         zh: '背景音频',
-//         url: 'background-audio/background-audio'
-//       }, {
-//         zh: '文件',
-//         url: 'file/file'
-//       }, {
-//         zh: '视频',
-//         url: 'video/video'
-//       }, {
-//         zh: '音视频合成',
-//         url: 'media-container/media-container'
-//       }, {
-//         zh: '动态加载字体',
-//         url: 'load-font-face/load-font-face'
-//       }]
-//     }, {
-//       id: 'location',
-//       name: '位置',
-//       open: false,
-//       pages: [{
-//         zh: '获取当前位置',
-//         url: 'get-location/get-location'
-//       }, {
-//         zh: '使用原生地图查看位置',
-//         url: 'open-location/open-location'
-//       }, {
-//         zh: '使用原生地图选择位置',
-//         url: 'choose-location/choose-location'
-//       }]
-//     }, {
-//       id: 'storage',
-//       name: '数据',
-//       pages: [{
-//         zh: '本地存储',
-//         url: 'storage/storage'
-//       }, {
-//         zh: '周期性更新',
-//         url: 'get-background-fetch-data/get-background-fetch-data'
-
-//       }, {
-//         zh: '数据预拉取',
-//         url: 'get-background-prefetch-data/get-background-prefetch-data'
-//       }],
-//     }, {
-//       id: 'worker',
-//       name: '多线程',
-//       url: 'worker/worker'
-//     }, {
-//       id: 'framework',
-//       name: '框架',
-//       pages: [{
-//         zh: '双向绑定',
-//         url: 'two-way-bindings/two-way-bindings',
-//       }, {
-//         zh: 'WXS',
-//         url: 'wxs/wxs'
-//       }, {
-//         zh: '屏幕旋转',
-//         url: 'resizable/resizable'
-//       }]
-//     }],
-//     isSetTabBarPage: false,
-//     theme: 'light'
-//   },
 //   onLoad() {
 //     this.setData({
 //       theme: wx.getSystemInfoSync().theme || 'light'
@@ -340,55 +92,11 @@ Page({
   // },
 
   data: {
-    list: [
-      {
-        id: 'view',
-        name: '视图容器',
-        open: false,
-        pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
-      }, {
-        id: 'content',
-        name: '基础内容',
-        open: false,
-        pages: ['text', 'icon', 'progress', 'rich-text']
-      }, {
-        id: 'form',
-        name: '表单组件',
-        open: false,
-        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea', 'editor']
-      }, {
-        id: 'nav',
-        name: '导航',
-        open: false,
-        pages: ['navigator']
-      }, {
-        id: 'media',
-        name: '媒体组件',
-        open: false,
-        pages: ['image', 'video', 'camera', 'live-pusher', 'live-player']
-      }, {
-        id: 'map',
-        name: '地图',
-        open: false,
-        pages: ['map', { appid: 'wxe3f314db2e921db0', name: '腾讯位置服务示例中心' }]
-      }, {
-        id: 'canvas',
-        name: '画布',
-        open: false,
-        pages: ['canvas-2d', 'webgl']
-      }, {
-        id: 'open',
-        name: '开放能力',
-        open: false,
-        pages: ['ad', 'open-data', 'web-view']
-      }, {
-        id: 'obstacle-free',
-        name: '无障碍访问',
-        open: false,
-        pages: ['aria-component']
-      }
-    ],
-    theme: 'light'
+    ifTask:0,
+    ifCost:1,
+    array: ['选项1', '选项2', '选项3'],
+    index: 0,
+    title:"Task"
   },
 
   onLoad() {
@@ -432,22 +140,24 @@ Page({
     //   return new Date("2023-1-11 "+a.time) - new Date("2023-1-11 "+b.time);
     // });
     var list = app.globalData.todayTasks;
+    console.log(list,"list");
     // 根据type分类统计 并且计算每一个type所用的总分钟数sumTime
     var gatherList = {};//{type:{sumTime,arr:[{},{}]},type2:{}}
     list.forEach(item => {
       if (gatherList[item.type]) {
         gatherList[item.type].arr.push(item);
         //计算当前耗时
-        var min = (new Date("2023-1-11 " + item.endtime) - new Date("2023-1-11 " + item.starttime)) / 1000 / 60;
+        var min = (new Date("2023-1-11 " + item.et) - new Date("2023-1-11 " + item.st)) / 1000 / 60;
         gatherList[item.type].sumTime += min;
       } else {
         gatherList[item.type] = { sumTime: 0, arr: [] };
         gatherList[item.type].arr.push(item);
         //计算当前耗时
-        var min = (new Date("2023-1-11 " + item.endtime) - new Date("2023-1-11 " + item.starttime)) / 1000 / 60;
+        var min = (new Date("2023-1-11 " + item.et) - new Date("2023-1-11 " + item.st)) / 1000 / 60;
         gatherList[item.type].sumTime += min;
       }
     });
+    console.log(gatherList,"gatherList");
     //根据gatherList计算每个type所占的角度
     var gCanvaslist = [];//[{type,ratio,color}]
     //首先计算全部的分钟数
@@ -467,7 +177,7 @@ Page({
       gCanvaslist[i]["startAngle"] = startAngle;
       gCanvaslist[i]["endAngle"] = endAngle;
     }
-
+    console.log(gCanvaslist,"gCanvaslist");
     //根据gatherListh绘制图形
     wx.createSelectorQuery()
       .select('#timeCanvas') // 在 WXML 中填入的 id
@@ -502,42 +212,27 @@ Page({
           ctx.fillStyle = app.globalData.colorobj[gCanvaslist[i].type];
           ctx.fill();
         }
-
-        // ctx.moveTo(100, 15)
-        // // ctx.strokeStyle('#AAAAAA')
-        // ctx.stroke();
-
-        // ctx.fontSize = 12;
-        // ctx.fillStyle ='black';
-        // ctx.fillText('0', 165, 78)
-        // ctx.fillText('0.5*PI', 83, 145)
-        // ctx.fillText('1*PI', 15, 78)
-        // ctx.fillText('1.5*PI', 83, 10)
-
-        // // Draw points
-        // ctx.beginPath()
-        // ctx.arc(100, 75, 2, 0, 2 * Math.PI)
-        // // ctx.fillStyle('lightgreen')
-        // ctx.fill()
-
-        // ctx.beginPath()
-        // ctx.arc(100, 25, 2, 0, 2 * Math.PI)
-        // // ctx.fillStyle('blue')
-        // ctx.fill()
-
-        // ctx.beginPath()
-        // ctx.arc(150, 75, 2, 0, 2 * Math.PI)
-        // // ctx.fillStyle('red')
-        // ctx.fill()
-
-        // // Draw arc
-        // ctx.beginPath()
-        // ctx.arc(100, 75, 50, 0, 1.5 * Math.PI)
-        // // ctx.strokeStyle('#333333')
-        // ctx.stroke()
-
-        // ctx.draw()
       })
 
+  },
+  changePageTask(){
+    this.setData({
+      ifTask:1,
+      ifCost:0,
+      title:"Task"
+    });
+  },
+  changePageCost(){
+    this.setData({
+      ifTask:0,
+      ifCost:1,
+      title:"Cost"
+    });
+  },
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    });
+    // 执行其他操作，根据选择的值做出相应的处理
   }
 })
--- DailyPro/miniprogram/page/task/task.js
+++ DailyPro/miniprogram/page/task/task.js
@@ -9,22 +9,26 @@ CustomPage({
     }
   },
   data: {
+    Type_Pic:{"娱乐":"phoneG","工作":"phoneG"},//type和图片对应的数据表
     ifTask:0,
     ifCost:1,
     calendar:[],
     cLChosen:"",
-    costs:[{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:1},{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:2},{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:3}],
+    costs:[{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:1,cost:200},{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:2,cost:200},{type:"娱乐",con:"Today",pic:"../common/img/phoneG.png",index:3,cost:20}],
     tasks:[{type:"Work",con:"that's all bullshit",st:"11:00",et:"12:00",i:0},{type:"Work",con:"that's all bullshit",st:"11:00",et:"12:00",i:1}],
     today:dateFormat('yyyy-MM-dd',new Date()),
     timePicker: null,
     dlgStTime: "08:00",
     dlgEtTime: "08:00",
-    adddlgShow: 0,
+    addTaskdlgShow: 0,
+    addCostdlgShow: 0,
     typeIndex: 0,
     startX:0,//滑动时的起始坐标
     typeArr: ["娱乐","工作","学习"],
     contents: [],
-    inputValue: ""
+    inputValue: "",
+    inputCon:"",
+    inputNum:""
   },
   changePageTask(){
     this.setData({
@@ -63,7 +67,14 @@ CustomPage({
   },
   //addDlg弹框的input事件
   onInput(event) {
-    this.data.inputValue = event.detail.value;
+    var type = event.currentTarget.dataset.type;
+    if(type == "costNum"){
+      this.data.inputNum = event.detail.value;
+    }else if(type=="costCon"){
+      this.data.inputCon = event.detail.value;
+    }else{
+      this.data.inputValue = event.detail.value;
+    }
   },
   //手指刚放到屏幕触发
   conTouchS: function (e) {
@@ -140,26 +151,59 @@ CustomPage({
     });
   },
   //悬浮addDLg确定按钮的click
-  addDlgBtn() {
-    var st = this.data.dlgStTime;
-    var et = this.data.dlgEtTime;
-    var type = this.data.typeArr[this.data.typeIndex];
-    var con = this.data.inputValue;
-    var cons = this.data.tasks;
-    this.data.tasks.push({ st,et, type, con, id: cons.length,conLeft:0,i:cons.length%3 });
-    app.globalData.todayTasks = cons;
-    this.setData({
-      adddlgShow: 0,
-      tasks: cons
-    });
+  addDlgBtn(event) {
+    var type = event.currentTarget.dataset.type;
+    if(type=="task"){
+      var st = this.data.dlgStTime;
+      var et = this.data.dlgEtTime;
+      var type = this.data.typeArr[this.data.typeIndex];
+      var con = this.data.inputValue;
+      var cons = this.data.tasks;
+      this.data.tasks.push({ st,et, type, con, id: cons.length,conLeft:0,i:cons.length%3 });
+      app.globalData.todayTasks = cons;
+      this.setData({
+        addTaskdlgShow: 0,
+        tasks: cons
+      });
+    }else{//Cost
+      var type = this.data.typeArr[this.data.typeIndex];
+      var con = this.data.inputCon;
+      var pic = "../common/img/"+this.data.Type_Pic[type]+".png";
+      var list = this.data.costs;
+      var cost = this.data.inputNum;
+      this.data.costs.push({ type, cost,pic,con, id: list.length,conLeft:0,i:list.length%3 });
+      app.globalData.todayCost = list;
+      this.setData({
+        addCostdlgShow: 0,
+        costs: list
+      });
+      console.log(list);
+    }
   },
   showAddDlg(event){
-    // var button = event.currentTarget.dataset.btn;
-    // if (button === "task") {
-    // } else if (button === "cost") {
-    // }
-    this.setData({
-      adddlgShow: 1
-    });
+    var button = event.currentTarget.dataset.btn;
+    if (button === "task") {
+      this.setData({
+        addTaskdlgShow: 1,
+        addCostdlgShow:0
+      });
+    } else if (button === "cost") {
+      this.setData({
+        addTaskdlgShow: 0,
+        addCostdlgShow:1
+      });
+    }
+  },
+  closeAddDlg(event){
+    var type=event.currentTarget.dataset.type;
+    if(type=="task"){
+      this.setData({
+        addTaskdlgShow:0
+      });
+    }else{
+      this.setData({
+        addCostdlgShow:0
+      });
+    }
   }
 })
